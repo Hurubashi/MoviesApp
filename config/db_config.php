1a4cafb74a36a7ac55965085be8a7e9b2e8fef64
Max Rybak <hurubashi@gmail.com>
2019-05-30 09:38:33 +0300

    charset=utf8

--- config/db_config.php
+++ config/db_config.php
@@ -4,4 +4,4 @@ $DB_PASSWORD = 'mikcer';
 $DB_HOST = 'localhost';
 $DB_NAME = 'movies';
 
-$DB_DSN = 'mysql:host=' . $DB_HOST . ';' . 'dbname=' . $DB_NAME;
+$DB_DSN = 'mysql:host=' . $DB_HOST . ';' . 'dbname=' . $DB_NAME . ';' . 'charset=utf8';
